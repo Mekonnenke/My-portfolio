5caa187a4d2ed13c1c64be807bf89d51c85622d0
Mekonnen Mohammed <90818220+Mekonnenke@users.noreply.github.com>
2023-01-12 13:17:18 -0500

    html changes

---
+++ New folder/assets/js/script.js
@@ -0,0 +1,269 @@
+
+let menu = document.getElementById('menu')
+let navLinks = document.getElementById('nav-links')
+
+menu.onclick = function(){
+    menu.classList.toggle('openmenu');
+    
+}
+
+menu.addEventListener('click', ()=> {
+    navLinks.classList.toggle('active')
+})
+
+
+
+  
+const $ = str => document.querySelector(str);
+const $$ = str => document.querySelectorAll(str);
+
+(function() {
+    if (!window.app) {
+        window.app = {};
+    }
+    app.carousel = {
+        removeClass: function(el, classname='') {
+            if (el) {
+                if (classname === '') {
+                    el.className = '';
+                } else {
+                    el.classList.remove(classname);
+                }
+                return el;
+            }
+            return;
+        },
+        reorder: function() {
+            let childcnt = $("#carousel").children.length;
+            let childs = $("#carousel").children;
+
+            for (let j=0; j< childcnt; j++) {
+                childs[j].dataset.pos = j;
+            }
+        },
+        move: function(el) {
+            let selected = el;
+
+            if (typeof el === "string") {
+            console.log(`got string: ${el}`);
+                selected = (el == "next") ? $(".selected").nextElementSibling : $(".selected").previousElementSibling;
+                console.dir(selected);
+            }
+
+            let curpos = parseInt(app.selected.dataset.pos);
+            let tgtpos = parseInt(selected.dataset.pos);
+
+            let cnt = curpos - tgtpos;
+            let dir = (cnt < 0) ? -1 : 1;
+            let shift = Math.abs(cnt);
+
+            for (let i=0; i<shift; i++) {
+                let el = (dir == -1) ? $("#carousel").firstElementChild : $("#carousel").lastElementChild;
+
+                if (dir == -1) {
+                    el.dataset.pos = $("#carousel").children.length;
+                    $('#carousel').append(el);
+                } else {
+                    el.dataset.pos = 0;
+                    $('#carousel').prepend(el);
+                }
+
+                app.carousel.reorder();
+            }
+
+
+            app.selected = selected;
+            let next = selected.nextElementSibling;// ? selected.nextElementSibling : selected.parentElement.firstElementChild;
+            var prev = selected.previousElementSibling; // ? selected.previousElementSibling : selected.parentElement.lastElementChild;
+            var prevSecond = prev ? prev.previousElementSibling : selected.parentElement.lastElementChild;
+            var nextSecond = next ? next.nextElementSibling : selected.parentElement.firstElementChild;
+
+            selected.className = '';
+            selected.classList.add("selected");
+
+            app.carousel.removeClass(prev).classList.add('prev');
+            app.carousel.removeClass(next).classList.add('next');
+
+            app.carousel.removeClass(nextSecond).classList.add("nextRightSecond");
+            app.carousel.removeClass(prevSecond).classList.add("prevLeftSecond");
+
+            app.carousel.nextAll(nextSecond).forEach(item=>{ item.className = ''; item.classList.add('hideRight') });
+            app.carousel.prevAll(prevSecond).forEach(item=>{ item.className = ''; item.classList.add('hideLeft') });
+
+        },
+        nextAll: function(el) {
+            let els = [];
+
+            if (el) {
+                while (el = el.nextElementSibling) { els.push(el); }
+            }
+
+            return els;
+
+        },
+        prevAll: function(el) {
+            let els = [];
+
+            if (el) {
+                while (el = el.previousElementSibling) { els.push(el); }
+            }
+
+
+            return els;
+        },
+        keypress: function(e) {
+            switch (e.which) {
+                case 37: // left
+                    app.carousel.move('prev');
+                    break;
+
+                case 39: // right
+                    app.carousel.move('next');
+                    break;
+
+                default:
+                    return;
+            }
+            e.preventDefault();
+            return false;
+        },
+        select: function(e) {
+        console.log(`select: ${e}`);
+            let tgt = e.target;
+            while (!tgt.parentElement.classList.contains('carousel')) {
+                tgt = tgt.parentElement;
+            }
+
+            app.carousel.move(tgt);
+
+        },
+        previous: function(e) {
+            app.carousel.move('prev');
+        },
+        next: function(e) {
+            app.carousel.move('next');
+        },
+        doDown: function(e) {
+        console.log(`down: ${e.x}`);
+            app.carousel.state.downX = e.x;
+        },
+        doUp: function(e) {
+        console.log(`up: ${e.x}`);
+            let direction = 0,
+                velocity = 0;
+
+            if (app.carousel.state.downX) {
+                direction = (app.carousel.state.downX > e.x) ? -1 : 1;
+                velocity = app.carousel.state.downX - e.x;
+
+                if (Math.abs(app.carousel.state.downX - e.x) < 10) {
+                    app.carousel.select(e);
+                    return false;
+                }
+                if (direction === -1) {
+                    app.carousel.move('next');
+                } else {
+                    app.carousel.move('prev');
+                }
+                app.carousel.state.downX = 0;
+            }
+        },
+        init: function() {
+            document.addEventListener("keydown", app.carousel.keypress);
+           // $('#carousel').addEventListener("click", app.carousel.select, true);
+            $("#carousel").addEventListener("mousedown", app.carousel.doDown);
+            $("#carousel").addEventListener("touchstart", app.carousel.doDown);
+            $("#carousel").addEventListener("mouseup", app.carousel.doUp);
+            $("#carousel").addEventListener("touchend", app.carousel.doup);
+
+            app.carousel.reorder();
+            $('#prev').addEventListener("click", app.carousel.previous);
+            $('#next').addEventListener("click", app.carousel.next);
+            app.selected = $(".selected");
+
+        },
+        state: {}
+
+    }
+    app.carousel.init();
+})();
+
+
+
+ // Set a variable for our button element.
+const scrollToTopButton = document.getElementById('scroll-button');
+
+// Let's set up a function that shows our scroll-to-top button if we scroll beyond the height of the initial window.
+const scrollFunc = () => {
+  // Get the current scroll value
+  let y = window.scrollY;
+  
+  // If the scroll value is greater than the window height, let's add a class to the scroll-to-top button to show it!
+  if (y > 0) {
+    scrollToTopButton.className = "scroll-down show ";
+  } else {
+    scrollToTopButton.className = "scroll-down hide";
+  }
+};
+
+window.addEventListener("scroll", scrollFunc);
+
+const scrollToTop = () => {
+  // Let's set a variable for the number of pixels we are from the top of the document.
+  const c = document.documentElement.scrollTop || document.body.section.scrollTop;
+  
+  // If that number is greater than 0, we'll scroll back to 0, or the top of the document.
+  // We'll also animate that scroll with requestAnimationFrame:
+  // https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+  if (c > 0) {
+    window.requestAnimationFrame(scrollToTop);
+    // ScrollTo takes an x and a y coordinate.
+    // Increase the '10' value to get a smoother/slower scroll!
+    window.scrollTo(0, c - c / 10);
+  }
+};
+
+// When the button is clicked, run our ScrolltoTop function above!
+scrollToTopButton.onclick = function(e) {
+  e.preventDefault();
+  scrollToTop();
+}
+ 
+//   cookies js
+
+const cookieContainer = document.querySelector('.cookie-container');
+const cookieButton = document.querySelector('.cookie-btn');
+
+setItem = (cName, cValue, expDays) =>{ 
+    let date = new Date();
+    date.setTime(date.getTime() +(expDays *24 * 60 *60 * 1000));
+    const expires = 'expires=' + date.toUTCString();
+    document.cookie = cName + '=' +  cValue + ';' + expires +"; path=/"
+}
+
+
+//to creat cookies and display in the localhost
+cookieButton.addEventListener("click", ()=>{
+    cookieContainer.classList.remove('active');
+    localStorage.setItem('cookies', true, 30)
+});
+setTimeout(function(){
+   if(!localStorage.getItem('cookies'))
+    cookieContainer.classList.add('active');
+}, 2000)
+
+
+// recaptcha
+var recaptcha_response = '';
+function submitUserForm() {
+    if(recaptcha_response.length == 0) {
+        document.getElementById('g-recaptcha-error').innerHTML = '<span style="color:red;">This field is required.</span>';
+        return false;
+    }
+    return true;
+}
+ 
+function verifyCaptcha(token) {
+    recaptcha_response = token;
+    document.getElementById('g-recaptcha-error').innerHTML = '';
+}
